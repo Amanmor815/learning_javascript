b081508403dcd94a660c272411a26e0f54ff214c
Aman Mor <126899034+Amanmor815@users.noreply.github.com>
2023-03-11 00:22:51 +0530

    Add files via upload

---
+++ 49_Strings.js
@@ -0,0 +1,8 @@
+var string="Sir, "
+var name ="Aman "
+var message ="is a good boy"
+var com = `${string} ${name} ${message}`
+console.log(com);
+var len = name.length;
+console.log(`Length of name is ${len}`)
+console.log("Hello \n Aman")
